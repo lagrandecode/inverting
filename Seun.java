8c18ec9dbb8a8d14511bbabcbe0223d23272d072
lagrandecode <seunogunmolufirst1@gmail.com>
2025-01-01 23:59:06 -0500

    "commit"

---
+++ Seun.java
@@ -0,0 +1,25 @@
+public class Seun{
+    public static void main(String[] args) {
+        int rows = 5;
+        for( int i = 1; i <=rows; i++){
+            System.out.print("*");
+
+        }
+        System.out.println();
+        System.out.print("*");
+        System.out.println();
+        for(int i = 1; i<=rows; i++){
+            System.out.print("*");
+        }
+        System.out.println("");
+        for(int i =1; i<rows; i++){
+            System.out.print(" ");
+        }
+        System.out.print("*");
+        System.out.println();
+        for (int i = 1; i <= rows; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
