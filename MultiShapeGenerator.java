e0279acf699fa8bb6f106d4c5353910803c2d758
lagrandecode <seunogunmolufirst1@gmail.com>
2025-01-09 01:10:28 -0500

    "commit

--- MultiShapeGenerator.java
+++ MultiShapeGenerator.java
@@ -1,3 +1,27 @@
+
+import java.util.Scanner;
+
 public class MultiShapeGenerator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Pattern Game");
+        System.err.println("Select 1, if you want Triangle");
+        System.err.println("Select 2, if you want Solid Pyramid");
+        System.err.println("Select 2, if you want Hallow Pyramid");
+        int choice = scanner.nextInt();
+        int rows = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+            
+                
+                break;
+            default:
+                throw new AssertionError();
+        }
+
+ 
+
+    }
     
 }
