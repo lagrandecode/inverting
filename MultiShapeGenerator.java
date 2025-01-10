2d99de21ee98a171b8f47ce9f1f46d3da3c55119
lagrandecode <seunogunmolufirst1@gmail.com>
2025-01-09 23:36:10 -0500

    "commit"

--- MultiShapeGenerator.java
+++ MultiShapeGenerator.java
@@ -13,7 +13,17 @@ public class MultiShapeGenerator {
 
         switch (choice) {
             case 1:
-            
+            for( int i = 1; i<= rows; i++){
+                for(int j = 1; j<=rows-i; j++){
+                    System.out.print(" ");
+
+                }
+                for(int j = 1; j<=i; j ++){
+                    System.out.print("*");;
+                }
+                System.out.println();
+            }
+
                 
                 break;
             default:
