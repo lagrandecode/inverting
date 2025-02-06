bd7a235fef3d71bdc3b3b7636a60e047c23b38bd
lagrandecode <seunogunmolufirst1@gmail.com>
2025-02-05 23:29:16 -0500

    "commit"

--- SpiralPattern.java
+++ SpiralPattern.java
@@ -2,11 +2,17 @@ public class SpiralPattern {
     public static void main(String[] args) {
         int n = 5;
         for(int i = 1; i<=n; i++){
-            for (int j = 1; j < n-i; j++){
+            for (int j = 1; j <= n-i; j++){
                 System.out.print(" ");  
             }
             for(int j = 1; j<=2*i-1; j++){
-                System.out.print("*");
+                if (j==1 || j==2*i-1) {
+                    System.out.print("*");
+                    
+                } else {
+                    System.out.print(" ");
+                }
+               
             }
 
             System.out.println(" ");
